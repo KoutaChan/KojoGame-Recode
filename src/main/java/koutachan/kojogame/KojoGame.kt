7b2329e40f266e5529e2c4ca4264715f60bc3544
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-22 10:12:55 +0900

    準備ー

---
+++ src/main/java/koutachan/kojogame/KojoGame.kt
@@ -0,0 +1,13 @@
+package koutachan.kojogame
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KojoGame : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
