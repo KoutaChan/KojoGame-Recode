109cb83b21e188cdb769bc5be532a3af7d4b7487
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-22 11:51:13 +0900

    プレイヤーが入るごとにrunTaskTimerが増える恐ろしさよ

--- src/main/java/koutachan/kojogame/runTask/ScoreBoard.kt
+++ src/main/java/koutachan/kojogame/runTask/ScoreBoard.kt
@@ -3,18 +3,24 @@ package koutachan.kojogame.runTask
 import koutachan.kojogame.KojoGame.Companion.plugin
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scoreboard.DisplaySlot
 
 
 object ScoreBoard {
     fun ScoreBoard(player: Player) {
-        Bukkit.getScheduler().runTaskTimer(plugin, Runnable {
-            val scoreboard = Bukkit.getScoreboardManager().getNewScoreboard()
-            val p = scoreboard.registerNewObjective(player.name, "dummy")
-            p.setDisplaySlot(DisplaySlot.SIDEBAR)
-            p.getScore("${player.getHealth()}").setScore(10)
-            player.setScoreboard(scoreboard)
-
-        }, 0, 10)
+        object : BukkitRunnable() {
+            override fun run() {
+                if (!player.isOnline) {
+                    cancel()
+                }
+                //PlayerBoard
+                val scoreboard = Bukkit.getScoreboardManager().getNewScoreboard()
+                val p = scoreboard.registerNewObjective(player.name, "dummy")
+                p.setDisplaySlot(DisplaySlot.SIDEBAR)
+                p.getScore("${player.getHealth()}").setScore(10)
+                player.setScoreboard(scoreboard)
+            }
+        }.runTaskTimer(plugin, 0, 1)
     }
 }
