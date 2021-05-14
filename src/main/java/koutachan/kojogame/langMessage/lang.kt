ce9e1c39bcec5bf1bb59f4249383dc9256bbf370
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-05-15 00:20:15 +0900

    ScoreBoard(Suffix)にカラーコードが適用されない問題を修正

--- src/main/java/koutachan/kojogame/langMessage/lang.kt
+++ src/main/java/koutachan/kojogame/langMessage/lang.kt
@@ -1,6 +1,7 @@
 package koutachan.kojogame.langMessage
 
 import koutachan.kojogame.*
+import org.bukkit.ChatColor
 import org.bukkit.configuration.file.YamlConfiguration
 
 object lang {
@@ -45,42 +46,52 @@ object lang {
     fun LengthCheck(){
         if(SCOREBOARD_LINE10.length in 17..32){
             SCOREBOARD_LINE10_SUFFIX = SCOREBOARD_LINE10.substring(16)
+            SCOREBOARD_LINE10_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE10) + SCOREBOARD_LINE10_SUFFIX
             SCOREBOARD_LINE10 = SCOREBOARD_LINE10.substring(0,16)
         }
         if(SCOREBOARD_LINE9.length in 17..32){
             SCOREBOARD_LINE9_SUFFIX = SCOREBOARD_LINE9.substring(16)
+            SCOREBOARD_LINE9_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE9) + SCOREBOARD_LINE9_SUFFIX
             SCOREBOARD_LINE9 = SCOREBOARD_LINE9.substring(0,16)
         }
         if(SCOREBOARD_LINE8.length in 17..32){
             SCOREBOARD_LINE8_SUFFIX = SCOREBOARD_LINE8.substring(16)
+            SCOREBOARD_LINE8_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE8) + SCOREBOARD_LINE8_SUFFIX
             SCOREBOARD_LINE8 = SCOREBOARD_LINE8.substring(0,16)
         }
         if(SCOREBOARD_LINE7.length in 17..32){
             SCOREBOARD_LINE7_SUFFIX = SCOREBOARD_LINE7.substring(16)
+            SCOREBOARD_LINE7_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE7) + SCOREBOARD_LINE7_SUFFIX
             SCOREBOARD_LINE7 = SCOREBOARD_LINE7.substring(0,16)
         }
         if(SCOREBOARD_LINE6.length in 17..32){
             SCOREBOARD_LINE6_SUFFIX = SCOREBOARD_LINE6.substring(16)
+            SCOREBOARD_LINE6_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE6) + SCOREBOARD_LINE6_SUFFIX
             SCOREBOARD_LINE6 = SCOREBOARD_LINE6.substring(0,16)
         }
         if(SCOREBOARD_LINE5.length in 17..32){
             SCOREBOARD_LINE5_SUFFIX = SCOREBOARD_LINE5.substring(16)
+            SCOREBOARD_LINE5_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE5) + SCOREBOARD_LINE5_SUFFIX
             SCOREBOARD_LINE5 = SCOREBOARD_LINE5.substring(0,16)
         }
         if(SCOREBOARD_LINE4.length in 17..32){
             SCOREBOARD_LINE4_SUFFIX = SCOREBOARD_LINE4.substring(16)
+            SCOREBOARD_LINE4_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE4) + SCOREBOARD_LINE4_SUFFIX
             SCOREBOARD_LINE4 = SCOREBOARD_LINE4.substring(0,16)
         }
         if(SCOREBOARD_LINE3.length in 17..32){
             SCOREBOARD_LINE3_SUFFIX = SCOREBOARD_LINE3.substring(16)
+            SCOREBOARD_LINE3_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE3) + SCOREBOARD_LINE3_SUFFIX
             SCOREBOARD_LINE3 = SCOREBOARD_LINE3.substring(0,16)
         }
         if(SCOREBOARD_LINE2.length in 17..32){
             SCOREBOARD_LINE2_SUFFIX = SCOREBOARD_LINE2.substring(16)
+            SCOREBOARD_LINE2_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE2) + SCOREBOARD_LINE2_SUFFIX
             SCOREBOARD_LINE2 = SCOREBOARD_LINE2.substring(0,16)
         }
         if(SCOREBOARD_LINE1.length in 17..32){
             SCOREBOARD_LINE1_SUFFIX = SCOREBOARD_LINE1.substring(16)
+            SCOREBOARD_LINE1_SUFFIX = ChatColor.getLastColors(SCOREBOARD_LINE1) + SCOREBOARD_LINE1_SUFFIX
             SCOREBOARD_LINE1 = SCOREBOARD_LINE1.substring(0,16)
         }
     }
